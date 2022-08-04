7b4944ae555be72b2e2b35c85b5fb1c9bf71f437
cabonilla <christian.bonilla01@gmail.com>
2022-08-04 14:42:47 -0700

    Changed favicon and title.

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,14 @@
+import Head from 'next/head';
 import MainInput from '../components/MainInput/index';
 import homeStyling from '../styles/Home.module.css';
 
 const Home = () => {
   return (
     <div className={homeStyling.container}>
+      <Head>
+        <title>CodeAlong</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
       <MainInput/>
     </div>
   )
